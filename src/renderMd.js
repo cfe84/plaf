c5962573a5a15cac8346fadd0a8dacb6dfd5dd3e
Charles Feval <chfeval@microsoft.com>
2020-05-05 23:17:26 -0700

    Add full content in context for rendering

--- src/renderMd.js
+++ src/renderMd.js
@@ -5,6 +5,7 @@ const renderMd = (content, outputFolder, deps) => {
     .filter(file => file.type === consts.fileType.md)
     .forEach(mdFile => {
       const template = deps.getTemplate(mdFile.properties);
+      mdFile.context = { content };
       const rendered = template(mdFile);
       const fileName = deps.path.join(outputFolder, mdFile.relativePath.replace(/\.md$/i, ".html"));
       deps.fs.writeFileSync(fileName, rendered)
