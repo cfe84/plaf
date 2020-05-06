9ca996996b2bedb7a3cfd74deef6a3733d25b668
Charles Feval <chfeval@microsoft.com>
2020-05-05 23:24:56 -0700

    Remove the non-tested context thing

--- src/renderMd.js
+++ src/renderMd.js
@@ -5,7 +5,6 @@ const renderMd = (content, outputFolder, deps) => {
     .filter(file => file.type === consts.fileType.md)
     .forEach(mdFile => {
       const template = deps.getTemplate(mdFile.properties);
-      mdFile.context = { content };
       const rendered = template(mdFile);
       const fileName = deps.path.join(outputFolder, mdFile.relativePath.replace(/\.md$/i, ".html"));
       deps.fs.writeFileSync(fileName, rendered)
